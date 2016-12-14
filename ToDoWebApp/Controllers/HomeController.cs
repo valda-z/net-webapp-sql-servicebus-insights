fcffcc18ac8606641d195ca9f4f7a5f3d9e1acb6
valda-z <vazvadsk@microsoft.com>
2016-12-14 16:51:25 +0100

    Project skeleton done
    Related Work Items: #29

--- ToDoWebApp/Controllers/HomeController.cs
+++ ToDoWebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoWebApp.Models;
 
 namespace ToDoWebApp.Controllers
 {
@@ -10,7 +11,7 @@ namespace ToDoWebApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new ToDoDBContext().ToDoes);
         }
 
         public ActionResult About()
@@ -18,6 +19,41 @@ namespace ToDoWebApp.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Detail(int id)
+        {
+            ToDo model;
+            if (id == 0)
+            {
+                model = new ToDo();
+            }
+            else
+            {
+                model = new ToDoDBContext().ToDoes.Single(e => e.Id == id);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Detail(int id, ToDo model)
+        {
+            var cx = new ToDoDBContext();
+            if (id == 0)
+            {
+                model.GId = Guid.NewGuid();
+                model.Created = DateTime.Now;
+                cx.ToDoes.Add(model);
+            }
+            else
+            {
+                var p = cx.ToDoes.Single(e => e.Id == id);
+                p.Note = model.Note;
+            }
+            cx.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult Contact()
---
+++ ToDoWebApp/Models/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDoWebApp.Models
+{
+    [Table("ToDo")]
+    public class ToDo
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public Guid GId { get; set; }
+
+        [StringLength(500)]
+        public string Note { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
